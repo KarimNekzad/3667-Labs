bb406c5d0f845ff03287e3ee5ba6f9a76c197eee
Karim Nekzad <karimnekzad00@gmail.com>
2021-10-28 21:10:45 -0400

    Added health UI for player and enemy.

--- Assets/Scripts/EnemyHealthTracker.cs
+++ Assets/Scripts/EnemyHealthTracker.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EnemyHealthTracker : MonoBehaviour
 {
     [SerializeField] private int _health = 5;
     [SerializeField] private int _defaultDamage = 1;
+    [SerializeField] private GameObject _heartPrefab;
+    [SerializeField] private Text _healthText;
+
+    private const float _heartDefaultX = 5.3f;
+    private const float _heartDefaultY = 4.3f;
+    private const float _heartXPositionOffset = 0.5f;
+    private GameObject[] _hearts = new GameObject[5];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _healthText.text = "Enemy HP: " + _health;
+
+        for (int i = 0; i < _health; i++)
+        {
+            GameObject heartInstance = Instantiate(_heartPrefab);
+            heartInstance.transform.position = new Vector3(_heartDefaultX + _heartXPositionOffset * i, _heartDefaultY, 0f);
+            _hearts[i] = heartInstance;
+        }
     }
 
     // Update is called once per frame
@@ -22,7 +37,14 @@ public class EnemyHealthTracker : MonoBehaviour
 
     public void TakeDamage()
     {
+        int rightmostHeartIndex = _health - 1;
+        if (rightmostHeartIndex >= 0 && rightmostHeartIndex <= 4)
+        {
+            Destroy(_hearts[rightmostHeartIndex]);
+        }
+
         _health -= _defaultDamage;
+        _healthText.text = "Enemy HP: " + _health;
 
         if (_health <= 0)
         {
--- Assets/Scripts/PlayerHealthTracker.cs
+++ Assets/Scripts/PlayerHealthTracker.cs
@@ -2,16 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealthTracker : MonoBehaviour
 {
     [SerializeField] private int _health = 5;
     [SerializeField] private int _defaultDamage = 1;
+    [SerializeField] private GameObject _heartPrefab;
+    [SerializeField] private Text _healthText;
+
+    private const float _heartDefaultX = -8;
+    private const float _heartDefaultY = 4.3f;
+    private const float _heartXPositionOffset = 0.5f;
+    private GameObject[] _hearts = new GameObject[5];
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _healthText.text = "Player HP: " + _health;
+
+        for (int i = 0; i < _health; i++)
+        {
+            GameObject heartInstance = Instantiate(_heartPrefab);
+            heartInstance.transform.position = new Vector3(_heartDefaultX + _heartXPositionOffset * i, _heartDefaultY, 0f);
+            _hearts[i] = heartInstance;
+        }
     }
 
     // Update is called once per frame
@@ -22,8 +37,16 @@ public class PlayerHealthTracker : MonoBehaviour
 
     public void TakeDamage()
     {
+        int rightmostHeartIndex = _health - 1;
+        if (rightmostHeartIndex >= 0 && rightmostHeartIndex <= 4)
+        {
+            Destroy(_hearts[rightmostHeartIndex]);
+        }
+
         _health -= _defaultDamage;
-        
+        _healthText.text = "Player HP: " + _health;
+
+
         if (_health <= 0)
         {
             //RestartLevel();
